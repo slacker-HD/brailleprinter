bcde63b3e1d4d00729884dee58c08afc649a33ac
slacker_HD <hudi@linuxmail.org>
2021-01-19 14:08:20 +0800

    开始写硬件方面的c程序

---
+++ assets/printer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <io.h>
+
+int main(int argc,char *argv[])
+{
+    FILE *fp;
+    char line[50];
+    if(arcg != 0)
+    {
+        printf("Please input the print file path as argument.\n");
+        return -1;
+    }
+    if (!access(argv[0],0))
+    {
+        printf("The print file does not exist.\n");
+        return -1;
+    }
+    fp=fopen(argv[0],"r");
+    if(fp == NULL)
+    {
+        printf("Can not load the print file.");
+        return -1;
+    }
+    while(!feof(fp))
+    {
+        fgets(line,1000,fp);
+        printf("%s",line);
+    }
+    fclose(fp);
+    return 0;
+}
