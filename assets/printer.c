c1e0cda2b2f664a214625640c758fd2e9f2d11bf
slacker_HD <hudi@linuxmail.org>
2021-01-20 16:51:18 +0800

    丰富下位机代码

--- assets/printer.c
+++ assets/printer.c
@@ -7,6 +7,109 @@
 #include <sys/io.h>
 #include <wiringSerial.h>
 #include <wiringPi.h>
+#include <Servo.h>
+#endif
+
+#define X_MAX 100       // 每行多少方格
+#define Y_MAX 10        // 多少行
+#define MOTOR1CTLPIN 8  // 电机1的控制引脚
+#define MOTOR1STEPPIN 2 // 电机1 _step
+#define MOTOR1DIRPIN 5  // 电机1转动方向
+#define MOTOR2CTLPIN 9  // 电机2的控制引脚
+#define MOTOR2STEPPIN 3 // 电机2 _step
+#define MOTOR2DIRPIN 6  // 电机2转动方向
+#define SERVOPIN 4      // 电机2转动方向
+#define SERVOVALUE 25   // 舵机每次转过的的角度，此数值涉及打洞的深度
+
+#ifdef __linux
+Servo _z_axis;          // 创建一个舵机控制对象
+#endif
+
+const int _step = 250;  // _step Length
+int _x_pos, _y_pos;     // 打印针头当前位置
+
+#ifdef __linux
+//控制舵机来回一次
+void holing()
+{
+    char pos;
+    for (pos = 0; pos < 25; pos += 1) // 从0度到180度运动
+    {                                 // 每次步进一度
+        _z_axis.write(pos);
+        delay(5);
+    }
+    for (pos = 25; pos >= 1; pos -= 1) //从180度到0度运动
+    {
+        _z_axis.write(pos);
+        delay(5);
+    }
+}
+//控制电机行走，需要根据硬件修改
+void xStep_forward(int Step)
+{
+    int x;
+    digitalWrite(5, HIGH);     // Set Dir low
+    digitalWrite(6, HIGH);     // Set Dir low
+    for (x = 0; x < Step; x++) // Loop 2000 times
+    {
+        digitalWrite(2, HIGH);  // Output high
+        digitalWrite(3, HIGH);  // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+        digitalWrite(2, LOW);   // Output low
+        digitalWrite(3, LOW);   // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+    }
+}
+//控制电机行走，需要根据硬件修改
+
+void xStep_backward(int Step)
+{
+    int x;
+    digitalWrite(5, LOW);      // Set Dir low
+    digitalWrite(6, LOW);      // Set Dir low
+    for (x = 0; x < Step; x++) // Loop 2000 times
+    {
+        digitalWrite(2, HIGH);  // Output high
+        digitalWrite(3, HIGH);  // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+        digitalWrite(2, LOW);   // Output low
+        digitalWrite(3, LOW);   // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+    }
+}
+//控制电机行走，需要根据硬件修改
+void yStep_forward(int Step)
+{
+    int x;
+    digitalWrite(5, HIGH);     // Set Dir high
+    digitalWrite(6, LOW);      // Set Dir high
+    for (x = 0; x < Step; x++) // Loop 200 times
+    {
+        digitalWrite(2, HIGH);  // Output high
+        digitalWrite(3, HIGH);  // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+        digitalWrite(2, LOW);   // Output low
+        digitalWrite(3, LOW);   // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+    }
+}
+//控制电机行走，需要根据硬件修改
+
+void yStep_backward(int Step)
+{
+    int x;
+    digitalWrite(5, LOW);      // Set Dir low
+    digitalWrite(6, HIGH);     // Set Dir low
+    for (x = 0; x < Step; x++) // Loop 2000 times
+    {
+        digitalWrite(2, HIGH);  // Output high
+        digitalWrite(3, HIGH);  // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+        digitalWrite(2, LOW);   // Output low
+        digitalWrite(3, LOW);   // Output high
+        delayMicroseconds(300); // Wait 1/2 a ms
+    }
+}
 #endif
 
 int main(int argc, char *argv[])
@@ -14,9 +117,6 @@ int main(int argc, char *argv[])
     FILE *fp;
     char line[24]; //应该最多只有12个的，乘以2以备以后扩展
     int i = 1;
-#ifdef __linux
-    wiringPiSetup();
-#endif
 
     if (argc != 2)
     {
@@ -30,6 +130,22 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+#ifdef __linux
+    wiringPiSetup();
+    //电机1的初始化
+    pinMode(MOTOR1CTLPIN, OUTPUT);    // 电机1的控制引脚
+    pinMode(MOTOR1STEPPIN, OUTPUT);   // 电机1 _step
+    pinMode(MOTOR1DIRPIN, OUTPUT);    // 电机1转动方向
+    //电机2的初始化
+    pinMode(MOTOR2CTLPIN, OUTPUT);    // 电机2的控制引脚
+    pinMode(MOTOR2STEPPIN, OUTPUT);   // 电机2 _step
+    pinMode(MOTOR2DIRPIN, OUTPUT);    // 电机2转动方向
+    //舵机初始化
+    _z_axis.attach(SERVOPIN);         // z pin
+    digitalWrite(MOTOR1CTLPIN, HIGH); // 开始控制电机1
+    digitalWrite(MOTOR2CTLPIN, HIGH); // 开始控制电机2
+    _z_axis.write(0);                 // 开始控制舵机，初始化位置归零
+#endif
     while (!feof(fp))
     {
         printf("input line %d:\n", i);
@@ -46,5 +162,10 @@ int main(int argc, char *argv[])
         }
     }
     fclose(fp);
+#ifdef __linux
+    digitalWrite(MOTOR1CTLPIN, LOW); // 停止控制电机1
+    digitalWrite(MOTOR2CTLPIN, LOW); // 停止控制电机2
+    _z_axis.write(0);                // 舵机位置归零
+#endif
     return 0;
 }
